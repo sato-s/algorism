9e44f2cdb5bd6babf544ab81312cbf54be859abd
Sato Sawahiko <sato.sawahiko@jp.fujitsu.com>
2019-12-10 14:29:25 +0900

    Failed? abbreviation

--- hacker_rank/dynamic_programing/abbreviation.rb
+++ hacker_rank/dynamic_programing/abbreviation.rb
@@ -1,49 +1,65 @@
-You can perform the following operations on the string, :
+# You can perform the following operations on the string, a:
+#
+# Capitalize zero or more of a's lowercase letters.
+# Delete all of the remaining lowercase letters in a.
+# Given two strings,  a and b, determine if it's possible to make  equal to  as described. If so, print YES on a new line. Otherwise, print NO.
+#
+# Sample Input
+#
+# 1
+# daBcd
+# ABC
+# Sample Output
+#
+# YES
+# Explanation
+#
+# image
+#
+# We have  daBcd and  ABC. We perform the following operation:
+#
+# Capitalize the letters a and c in  so that  dABCd.
+# Delete all the remaining lowercase letters in  so that  ABC.
+# Because we were able to successfully convert  to , we print YES on a new line.
+#
+require 'pry'
 
-Capitalize zero or more of 's lowercase letters.
-Delete all of the remaining lowercase letters in .
-Given two strings,  and , determine if it's possible to make  equal to  as described. If so, print YES on a new line. Otherwise, print NO.
+def possible?(s1, s2)
+  s1 = s1.chars
+  s2 = s2.chars
+  memo = Array.new(s1.size + 1) { Array.new(s2.size + 1) }
+  su1 = []
+  count = 0
+  s1.map{|s| !!(s.match /[A-Z]/) }.each do |is_upper_case|
+    if is_upper_case
+      count += 1
+    end
+    su1 << count
+  end
 
-For example, given  and , in  we can convert  and delete  to match . If  and , matching is not possible because letters may only be capitalized or discarded, not changed.
+  (0..(s1.size)).each do |i|
+    memo[i][0] = (su1[i] == 0)
+  end
 
-Function Description
+  (0..(s2.size)).each do |j|
+    memo[0][j] = (j == 0)
+  end
 
-Complete the function  in the editor below. It must return either  or .
+  (0..(s1.size)).each do |i|
+    (0..(s2.size)).each do |j|
+      if s1[i].upcase == s2[j]
+        memo[i][j] = memo[i-1][j-1]
+      else
+        if s1[i].match(/[A-Z]/)
+          # upcase
+        else
+        end
+      end
+    end
+  end
 
-abbreviation has the following parameter(s):
+  binding.pry
 
-a: the string to modify
-b: the string to match
-Input Format
+end
 
-The first line contains a single integer , the number of queries.
-
-Each of the next  pairs of lines is as follows: 
-- The first line of each query contains a single string, . 
-- The second line of each query contains a single string, .
-
-Constraints
-
-String  consists only of uppercase and lowercase English letters, ascii[A-Za-z].
-String  consists only of uppercase English letters, ascii[A-Z].
-Output Format
-
-For each query, print YES on a new line if it's possible to make string  equal to string . Otherwise, print NO.
-
-Sample Input
-
-1
-daBcd
-ABC
-Sample Output
-
-YES
-Explanation
-
-image
-
-We have  daBcd and  ABC. We perform the following operation:
-
-Capitalize the letters a and c in  so that  dABCd.
-Delete all the remaining lowercase letters in  so that  ABC.
-Because we were able to successfully convert  to , we print YES on a new line.
+possible?('daBcd', 'ABC')
