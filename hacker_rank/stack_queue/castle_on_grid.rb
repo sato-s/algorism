6b4be62d06c5b8cf34c09084270667a9549d7a79
Sato Sawahiko <sato.sawahiko@jp.fujitsu.com>
2019-12-13 16:52:29 +0900

    wip

---
+++ hacker_rank/stack_queue/castle_on_grid.rb
@@ -0,0 +1,6 @@
+# You are given a square grid with some cells open (.) and some blocked (X).
+# playing piece can move along any row or column until it reaches the edge of the grid or a blocked cell.
+# Given a grid, a start and an end position, determine the number of moves it will take to get to the end position.
+#
+# For example, you are given a grid with sides  described as follows:
+# 
--- hacker_rank/stack_queue/min_max_riddle.rb
+++ hacker_rank/stack_queue/min_max_riddle.rb
@@ -10,9 +10,16 @@
 #
 # 12 2 1 1
 require 'pry'
-arr = [2, 6, 1, 3, 2, 0]
+# arr = [2, 6, 1, 3, 0]
+arr = [2, 6, 1, 12]
+arr = %w[1 2 3 5 1 13 3].map(&:to_i)
+
+arr = %w|338861636 746392954 307196980 99067465 558432115 227421873 794286875 723412073 367233970 106205871 827272521 769842060 731038313 210163756 14762131 524078854 996373388 364749541 555468976 76225538 228965926 669746786 65840090 823516800 168768899 173445545 970027207 390557850 983670014 72348381 132245024 998742114 973368515 572064066 695638860 490405718 844068132 545204755 327755108 909260844 733712560 400505846 242522726 565969843 61327665 908018319 972088061 957574905 487935283 754595437 964446775 21609294 248550165 546865187 47751807 922684557 400325186 918259166 217214762 821293848 542409791 24229773 704353370 747554744 154606034 394698021 113758632 361606437 288102830 728428074 266846128 828133198 707337820 288568226 894829092 59190652 905527200 582774491 263478437 872923776 363759675 633497945 846898051 569011891 3251607 457997069 239086919 262332508 27443470 582018960 394231465 888400883 813847060 976964155 515615646 431824405 377146718 587447608 755690611 543236620 896772406 4739829 412893149 872000965 649106700 796002123 173268471 716918241 729662697 164844792 633217447 803925919 877736709 576599945 522438731 674161217 423481793 119633641 865849708 974269088 535541596 811353179 387094873 51178535 945667894 906922732 59484739 742719125 382946053 564400412 746284364 606053715 584988704 400577274 589976668 247157665 271283578 317021226 798543140 265419552 609106691 341069751 124295549 625251506|.map(&:to_i)
 
 def riddle(arr)
+  # Append (almost) minus infinity here
+  arr << -10 ** 100
+
   stack = []
   hash = {}
   (0...(arr.size)).each do |i|
@@ -21,47 +28,47 @@ def riddle(arr)
     elsif arr[i-1] <= arr[i]
       stack << i
     else
-      count = 1
-      v = -1000
-      while !stack.empty?
-        do
-        j = stack.last
-        if arr[j] < arr[i]
-          break;
+      while !stack.empty? do
+        break if arr[stack.last] < arr[i]
+        j = stack.pop
+        v = arr[j]
+        # binding.pry if v == 2
+        if stack.empty?
+          hash[v] = i - 0
+        else
+          # hash[v] = #j - stack.last
+          hash[v] = i - stack.last - 1
         end
-        # j = stack.pop
-        # v = arr[j]
-        # hash[v] = count
-        # count += 1
       end
       stack << i
     end
   end
-
-  count = 1
-  until stack.empty? do
-    v = stack.pop
-    hash[v] = count
-    count += 1
+  summary = {}
+  hash.each_pair do |k,v|
+    if summary[v]
+      if summary[v] < k
+        summary[v] = k
+      end
+    else
+      summary[v] = k
+    end
   end
 
   # Calc summary
-  summary = []
-  (1..(arr.size)).each do |i|
-    candidates = []
-    hash.each_pair do |k,v|
-      if v == i
-        candidates << k
-      end
-    end
-    unless candidates.empty?
-      summary << candidates.max
+  result = []
+  prev = nil
+  (1..(arr.size - 1)).to_a.reverse.each do |i|
+    if summary[i]
+      prev = summary[i]
+      result << summary[i]
     else
-      summary << summary[i - 1]
+      result << prev
     end
   end
+  result.reverse!
+
   binding.pry
+  return result
 end
 
 p riddle(arr)
-
